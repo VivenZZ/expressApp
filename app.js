ebdf8ba161a677ab832f86c2de707ab3d6e5d22b
VivenZZ <17706500512@163.com>
2019-10-19 16:04:08 +0800

    res.redirect 重定向

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ var express = require('express');
 var http = require('http');
 var app = express();
 
+// 重定向的请求要在发送headers之前，不然会报错。
+app.get("/about", (req, res) => {
+    // 重定向
+    res.redirect("/hello/admin");
+});
 // 所有的请求都通过这里
 app.all('*', (req, res, next) => {
     res.writeHead(200, {"Content-Type": "text/plain"});
     next();
 });
 app.get("/", (req, res) => res.end("home page!"));
-app.get("/about", (req, res) => res.end("about page!"));
 // 带参数的请求
 // app.get("/hello/:who", (req, res) => res.end(req.params.who));
 // 带参数的请求可选
