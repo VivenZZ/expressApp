70e7d2966042583a4efde1833a91c71f52600097
VivenZZ <17706500512@163.com>
2019-10-19 15:12:09 +0800

    中间件

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 var express = require('express');
 var app = express();
-var routers = require('./routers')(app);
+var routers = require('./routers');
 app.use(express.static(__dirname + '/public'));
-
-app.listen(8080);
+app.use((req, res, next) => {
+    if (req.url == "/") {
+        res.writeHead(200, {"Content-Type": "text/plain"});
+        res.end("Welcome to the homepage!\n");
+    } else {
+        next();
+    }
+});
+app.use((req, res, next) => {
+    if (req.url == "/about") {
+        res.writeHead(200, {"Content-Type": "text/plain"});
+        res.end("Welcome to the aboutpage!\n");
+    } else {
+        next();
+    }
+});
+routers(app);
+app.use((req, res) => {
+    res.writeHead(404, {"Content-Type": "text/plain"});
+    res.end("404 error!\n");
+});
+app.listen(1337);
